6d14bf9010ffad75e0fce25885b77fce8ef1fc50
HoWingYip <sploooosh884@gmail.com>
2018-10-15 12:18:06 +0800

    Finished React tic-tac-toe tutorial!

--- tutorial/my-app/src/index.js
+++ tutorial/my-app/src/index.js
@@ -51,11 +51,12 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if(calculateWinner(squares) || squares[i]) {
@@ -67,6 +68,7 @@ class Game extends React.Component {
         squares: squares,
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
     });
     /*
     remember - for this bit you didn't understand, updating
@@ -74,17 +76,25 @@ class Game extends React.Component {
     */
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ?
         "Go to move #" + move :
         "Go to game start";
+        //any non-zero value is taken to be equivalent to true
         return(
-          <li>
+          <li key = {move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
         );
@@ -102,12 +112,12 @@ class Game extends React.Component {
         <div className="game-board">
           <Board 
             squares = {current.squares}
-
+            onClick = {(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{{moves}}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -140,5 +150,3 @@ function calculateWinner(squares) {
   }
   return null;
 }
-
-//stopped at https://reactjs.org/tutorial/tutorial.html#picking-a-key
